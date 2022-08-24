a168cd52eb7d58a190b8ae051f1d048984f47428
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2022-08-24 17:24:03 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -47,7 +47,7 @@ function Skills() {
     ]
 
   return (
-    <div name="Skills" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+    <div name="Skills" class="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-full">
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-red-700'>
